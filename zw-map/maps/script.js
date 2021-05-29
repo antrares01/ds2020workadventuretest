fef25003d7e3bf2c0bcb239a8358db7e7f7f8854
antrares <antrares@c3d2.de>
2021-05-29 11:02:30 +0200

    more fixes

--- zw-map/maps/script.js
+++ zw-map/maps/script.js
@@ -16,7 +16,7 @@ WA.onEnterZone(zoneStageName, () => {
 })
 
 WA.onEnterZone(zoneWelcomeName, () => {
-    currentPopup =  WA.openPopup("popUpWelcome","Herzlich Willkommen beim IZ-Festival!",[]);
+    currentPopup =  WA.openPopup("popUpWelcome","Wunderschön das Du beim IZ-Festival dabei bist!",[]);
 })
 
 WA.onLeaveZone(zoneStageName, closePopUp)
