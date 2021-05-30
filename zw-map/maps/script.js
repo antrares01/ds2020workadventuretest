7b0240a3f4a47518c9a1f3c9c4c79164f70b4daa
antrares <antrares@c3d2.de>
2021-05-30 18:44:09 +0200

    more fixes

--- zw-map/maps/script.js
+++ zw-map/maps/script.js
@@ -1,7 +1,22 @@
 var zoneStageName = "popupStageZone";
 var zoneWelcomeName = "popupWelcomeZone";
+var zoneBBB1Name = "popupBBB1Zone";
+var zoneBBB2Name = "popupBBB2Zone";
+var zoneBBB3Name = "popupBBB3Zone";
+var zoneBBB4Name = "popupBBB4Zone";
+var zoneBBB5Name = "popupBBB5Zone";
+var zoneBBB6Name = "popupBBB6Zone";
 
 var urlStage = "https://bbb010hz.makeutopia.de/b/and-jww-q3s-6bf";
+
+
+var urlBBB1 = "https://bbb010hz.makeutopia.de/b/and-eu3-vzx-wcv";
+var urlBBB2 = "https://bbb010hz.makeutopia.de/b/and-mif-xy3-av1";
+var urlBBB3 = "https://bbb010hz.makeutopia.de/b/and-vqx-pox-d8t";
+var urlBBB4 = "https://bbb010hz.makeutopia.de/b/and-y3f-ywt-3l6";
+var urlBBB5 = "https://bbb010hz.makeutopia.de/b/and-wpa-hmu-l25";
+var urlBBB6 = "https://bbb010hz.makeutopia.de/b/and-iwb-hin-d1c";
+
 var currentPopup = undefined;
 
 WA.onEnterZone(zoneStageName, () => {
@@ -15,13 +30,84 @@ WA.onEnterZone(zoneStageName, () => {
         }]);
 })
 
+WA.onEnterZone(zoneBBB1Name, () => {
+   currentPopup =  WA.openPopup("popUpBBB1","Drücke 'OPEN' um in den Raum 1 zu kommen! (Pop-up-Fenster müssen im Browser erlaubt sein) ",[
+        {
+            label: "BBB1",
+            className: "popUpElement",
+            callback: (popup => {
+                WA.openTab(urlBBB1);
+            })
+        }]);
+})
+
+WA.onEnterZone(zoneBBB2Name, () => {
+   currentPopup =  WA.openPopup("popUpBBB2","Drücke 'OPEN' um in den Raum 2 zu kommen! (Pop-up-Fenster müssen im Browser erlaubt sein) ",[
+        {
+            label: "BBB2",
+            className: "popUpElement",
+            callback: (popup => {
+                WA.openTab(urlBBB2);
+            })
+        }]);
+})
+
+WA.onEnterZone(zoneBBB3Name, () => {
+   currentPopup =  WA.openPopup("popUpBBB3","Drücke 'OPEN' um in den Raum 3 zu kommen! (Pop-up-Fenster müssen im Browser erlaubt sein) ",[
+        {
+            label: "BBB3",
+            className: "popUpElement",
+            callback: (popup => {
+                WA.openTab(urlBBB3);
+            })
+        }]);
+})
+
+WA.onEnterZone(zoneBBB4Name, () => {
+   currentPopup =  WA.openPopup("popUpBBB4","Drücke 'OPEN' um in den Raum 4 zu kommen! (Pop-up-Fenster müssen im Browser erlaubt sein) ",[
+        {
+            label: "BBB4",
+            className: "popUpElement",
+            callback: (popup => {
+                WA.openTab(urlBBB4);
+            })
+        }]);
+})
+
+WA.onEnterZone(zoneBBB5Name, () => {
+   currentPopup =  WA.openPopup("popUpBBB5","Drücke 'OPEN' um in den Raum 5 zu kommen! (Pop-up-Fenster müssen im Browser erlaubt sein) ",[
+        {
+            label: "BBB5",
+            className: "popUpElement",
+            callback: (popup => {
+                WA.openTab(urlBBB5);
+            })
+        }]);
+})
+
+WA.onEnterZone(zoneBBB6Name, () => {
+   currentPopup =  WA.openPopup("popUpBBB6","Drücke 'OPEN' um in den Raum 6 zu kommen! (Pop-up-Fenster müssen im Browser erlaubt sein) ",[
+        {
+            label: "BBB6",
+            className: "popUpElement",
+            callback: (popup => {
+                WA.openTab(urlBBB6);
+            })
+        }]);
+})
+
 WA.onEnterZone(zoneWelcomeName, () => {
     currentPopup =  WA.openPopup("popUpWelcome","Herzlich Willkommen and Welcome!",[]);
 })
 
 WA.onLeaveZone(zoneStageName, closePopUp)
-
 WA.onLeaveZone(zoneWelcomeName, closePopUp)
+WA.onLeaveZone(zoneBBB1Name, closePopUp)
+WA.onLeaveZone(zoneBBB2Name, closePopUp)
+WA.onLeaveZone(zoneBBB3Name, closePopUp)
+WA.onLeaveZone(zoneBBB4Name, closePopUp)
+WA.onLeaveZone(zoneBBB5Name, closePopUp)
+WA.onLeaveZone(zoneBBB6Name, closePopUp)
 
 function closePopUp(){
     if (currentPopup !== undefined) {
